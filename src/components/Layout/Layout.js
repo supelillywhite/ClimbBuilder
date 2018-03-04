0b27beaad89b28279da7ccb84abfa68dce12a5e6
Jerel Lillywhite <supelillywhite@gmail.com>
2018-03-03 18:50:21 -0700

    Fixed issues in CSS styling by resetting the webpack config files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import classes from './Layout.css';
 const layout = ( props ) => (
   <Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className="classes.Content">
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Fragment>
